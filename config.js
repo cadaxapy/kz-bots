5d526440157ed7673c40cef8081358b5f3a5ff9f
elaman <elaman.bazarov98@gmail.com>
2017-06-15 17:46:39 +0600

    done

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   production: {
     url: 'https://api.namba1.co/chats',
-    token: 'df'
+    token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ODY5ODc0OTIzLCJwaG9uZSI6IjM5MTYiLCJwYXNzd29yZCI6IiQyYSQxMCRtNGNHQS5jelNSRWRjcHB4NFpnZnRlY0pzNGxjU3pTVTNnS1MvVkxRMlpod0NYcWZqQWZ2dSIsImlzQm90Ijp0cnVlLCJjb3VudHJ5Ijp0cnVlLCJpYXQiOjE0OTc1MTk0MTZ9.vjO86afcSk-FS_u7xyZOsH7Htv9iwaW9k41ZfAKzdoQ'
   },
   staging: {
     url: 'http://77.235.20.133:3000/chats',
