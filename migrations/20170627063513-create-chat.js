dc9cf866c2e4c26d462800530af52c26551ee1dc
elaman <elaman.bazarov98@gmail.com>
2017-06-27 17:10:30 +0600

    done

--- migrations/20170627063513-create-chat.js
+++ migrations/20170627063513-create-chat.js
@@ -18,7 +18,7 @@ module.exports = {
         allowNull: false
       },
       status: {
-        type: Sequelize.INEGER,
+        type: Sequelize.INTEGER,
         defaultValue: 0
       },
       name: {
